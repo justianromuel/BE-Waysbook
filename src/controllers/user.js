4770b228030265d0451a6e46a6ded99ebe58e1b0
justianromuel <jastinromuel@gmail.com>
2022-07-06 22:00:43 +0700

    first commit

---
+++ src/controllers/user.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+
+// Controller
+// const { getUsers} = require('../controllers/user')
+
+// Route
+// router.get('/users', getUsers)
+
+module.exports = router
