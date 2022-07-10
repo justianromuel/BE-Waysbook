c12ffafa1c462e9d32ecc1e2b1d08e00ac471050
justianromuel <jastinromuel@gmail.com>
2022-07-11 06:27:39 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,11 +1,115 @@
-const express = require('express')
+const { user } = require('../../models')
 
-const router = express.Router()
+exports.getUsers = async (req, res) => {
+    try {
+        const users = await user.findAll({
+            attributes: {
+                exclude: ["password", "createdAt", "updatedAt"],
+            },
+        })
 
-// Controller
-// const { getUsers} = require('../controllers/user')
+        res.send({
+            status: "success",
+            data: {
+                users,
+            },
+        })
+    } catch (error) {
+        console.log(error)
+        res.send({
+            status: "failed",
+            message: "Server Error",
+        })
+    }
+}
 
-// Route
-// router.get('/users', getUsers)
+exports.getUser = async (req, res) => {
+    try {
+        const { id } = req.params
 
-module.exports = router
+        const data = await user.findOne({
+            where: {
+                id,
+            },
+            attributes: {
+                exclude: ["password", "createdAt", "updatedAt"],
+            }
+        })
+
+        res.send({
+            status: "success",
+            data: {
+                user: data
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        res.send({
+            status: "failed",
+            message: "Server Error",
+        })
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const dataExist = await user.findOne({
+            where: { id }
+        })
+
+        if (!dataExist) {
+            return res.send({
+                message: `User with id: ${id} not found!`
+            })
+        }
+
+        await user.update(req.body, {
+            where: { id }
+        })
+
+        res.send({
+            status: 'success',
+            message: `Update user id: ${id} success`,
+            data: req.body
+        })
+    } catch (error) {
+        console.log(error)
+        res.send({
+            status: "failed",
+            message: "Server Error",
+        })
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const dataExist = await user.findOne({
+            where: { id }
+        })
+
+        if (!dataExist) {
+            return res.send({
+                message: `User with id: ${id} not found!`
+            })
+        }
+
+        await user.destroy({
+            where: { id }
+        })
+
+        res.send({
+            status: 'success',
+            message: `Delete user id: ${id} success`
+        })
+    } catch (error) {
+        console.log(error)
+        res.send({
+            status: "failed",
+            message: "Server Error",
+        })
+    }
+}
